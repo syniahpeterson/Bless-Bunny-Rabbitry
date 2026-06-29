8565276784666f0fb576785ff0247de44e53b482
syniahpeterson <syniahpeterson@gmail.com>
2026-06-29 19:01:10 -0400

    feat: add useScrollToTop hook for automatic scrolling to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+// Component Imports:
 import Navbar from "./components/layout/Navbar";
 import AppRouter from "./routes/AppRouter";
 import Footer from "./components/layout/Footer";
 
+// Hook Imports:
+import useScrollToTop from "./hooks/useScrollToTop";
+
 export default function App() {
+  useScrollToTop();
   return (
     <>
       <Navbar />
